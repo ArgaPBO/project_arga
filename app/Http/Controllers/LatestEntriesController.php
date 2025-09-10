154cf10866845f23f907d9c265950817a554f167
@argaathallah <argaathallah@gmail.com>
2025-09-10 13:54:37 +0700

    pertemuan 4.1

--- app/Http/Controllers/LatestEntriesController.php
+++ app/Http/Controllers/LatestEntriesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LatestEntries extends Controller
+class LatestEntriesController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\LatestEntriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'LatestEntries@index');
+Route::get('/home', [LatestEntriesController::class, 'index']);
